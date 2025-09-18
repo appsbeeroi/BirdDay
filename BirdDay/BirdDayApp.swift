6a09dd82899952064e7ae0ca1fd43fb19ac70fd8
esplusnin <esplusnin94@yandex.ru>
2025-09-18 18:39:04 +0300

    Initial Commit

---
+++ BirdDay/BirdDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdDayApp.swift
+//  BirdDay
+//
+//  Created by Евгений on 18.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct BirdDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
